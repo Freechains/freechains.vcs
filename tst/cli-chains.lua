59009be361df2b17a19c815ad69aced18b64caba
Francisco <francisco.santanna@gmail.com>
2026-03-05 14:58:40 -0300

    - empty user.name/email ("-"), refactor tests

--- tst/cli-chains.lua
+++ tst/cli-chains.lua
@@ -7,11 +7,15 @@ local GEN  = TMP .. "/genesis.lua"
 local ROOT = TMP .. "/root/"
 
 function exec (cmd)
-    print(cmd)
+    --print(cmd)
     local h = io.popen(cmd .. " 2>&1")
     local out = h:read("a"):match("^%s*(.-)%s*$")
     local ok, _, code = h:close()
     return out, (ok and 0 or code)
+end
+
+function TEST (name)
+    print("  - " .. name .. "... ")
 end
 
 -- setup
@@ -33,80 +37,70 @@ f:close()
 do
     print("==> freechains chains add lua")
 
-    do -- success
-        io.write('', "success... ")
+    do
+        TEST "success"
         local out, code = exec (
             EXE .. " --root " .. ROOT .. " chains add mychain lua " .. GEN
         )
         assert(code == 0, "exit code: " .. tostring(code))
         assert(#out == 40, "hash length: " .. #out)
         assert(out:match("^%x+$"), "hash is hex")
-        print("OK")
     end
 
-    do -- genesis file exists
-        io.write("  genesis file exists... ")
+    do
+        TEST "genesis file exists"
         local out = exec (
             "cat " .. ROOT .. "chains/mychain/.genesis.lua"
         )
         local genesis = load(out)()
         assert(type(genesis) == "table")
         assert(genesis.type == "#")
-        print("OK")
     end
 
-    do -- symlink points to hash dir
-        io.write("  symlink points to hash dir... ")
+    do
+        TEST "symlink points to hash dir"
         local target = exec (
             "readlink " .. ROOT .. "chains/mychain"
         )
         assert(target:match("^%x+/$"),
             "symlink target: " .. target)
-        print("OK")
     end
 
-    do -- bad genesis file
-        io.write("  bad genesis file... ")
+    do
+        TEST "bad genesis file"
         local _, code = exec (
             EXE .. " --root " .. ROOT .. " chains add bad lua " .. bad_file
         )
         assert(code ~= 0, "should fail")
-        print("OK")
     end
 
-    print()
-    print("chains rem (not implemented)")
-
-    do -- rem fails
-        io.write("  rem fails... ")
-        local _, code = exec (
-            EXE .. " --root " .. ROOT .. " chains rem mychain"
-        )
-        assert(code ~= 0, "should fail")
-        print("OK")
-    end
-
-    print()
-    print("chains add args/remote (not implemented)")
-
-    do -- add args fails
-        io.write("  add args fails... ")
+    do
+        TEST "add args fails"
         local _, code = exec (
             EXE .. " --root " .. ROOT .. " chains add x args --type '#'"
         )
         assert(code ~= 0, "should fail")
-        print("OK")
     end
 
-    do -- add remote fails
-        io.write("  add remote fails... ")
+    do
+        TEST "add remote fails"
         local _, code = exec (
             EXE .. " --root " .. ROOT .. " chains add x remote host hash"
         )
         assert(code ~= 0, "should fail")
-        print("OK")
     end
 end
 
-print()
-print("ALL PASSED")
+do
+    print("==> freechains chains rem")
+
+    do
+        TEST "rem fails"
+        local _, code = exec (
+            EXE .. " --root " .. ROOT .. " chains rem mychain"
+        )
+        assert(code ~= 0, "should fail")
+    end
+end
+
+print("<== ALL PASSED")
