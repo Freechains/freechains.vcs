b6ffcd9eaa0a7211ead75c7609c26155489f0ba1
Francisco Sant'Anna <francisco.santanna@gmail.com>
2026-03-14 08:07:07 -0300

    ...

--- src/constants.lua
+++ src/constants.lua
@@ -3,29 +3,26 @@ local min = 60 * s
 local h   = 60 * min
 
 return {
-    -- timestamp validation
-    TOLERANCE = 1*h,            -- max clock drift
-
-    -- reputation scale
-    SCALE = 1000,               -- 1 ext rep = 1000 internal
-
-    -- reputation limits
-    PIONEER_TOTAL = 30000,      -- 30 ext split among pioneers
-    POST_COST     = 1000,       -- 1 ext per signed post
-    MAX_REPS      = 30000,      -- 30 ext cap per author
-
-    -- like transfer
-    LIKE_TAX   = 10,            -- 10% burned on likes
-    LIKE_SPLIT = 2,             -- 50/50 split (divisor)
-
-    -- revocation
-    DISLIKE_MIN = 3,            -- min dislikes for revocation
-
-    -- post size
-    POST_SIZE = 131072,         -- 128 KB max payload
-
-    -- time windows
-    DISCOUNT_MAX  = 12*h,       -- max discount period
-    CONSOLIDATION = 24*h,       -- consolidation window
-    HARD_FORK     = 7*24*h,     -- branch divergence limit
+    time = {
+        tolerance     = 1*h,        -- max clock drift
+        discount      = 12*h,       -- max discount period
+        consolidation = 24*h,       -- consolidation window
+        hardfork      = 7*24*h,     -- branch divergence limit
+    },
+    reps = {
+        scale   = 1000,             -- 1 ext rep = 1000 internal
+        pioneer = 30000,            -- 30 ext split among pioneers
+        cost    = 1000,             -- 1 ext per signed post
+        max     = 30000,            -- 30 ext cap per author
+    },
+    like = {
+        tax   = 10,                 -- 10% burned on likes
+        split = 2,                  -- 50/50 split (divisor)
+    },
+    dislike = {
+        min = 3,                    -- min dislikes for revocation
+    },
+    post = {
+        size = 131072,              -- 128 KB max payload
+    },
 }
