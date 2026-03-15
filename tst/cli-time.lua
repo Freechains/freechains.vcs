96a15d44944716c5b8c9479fa09a64612602d364
Francisco Sant'Anna <francisco.santanna@gmail.com>
2026-03-15 06:44:35 -0300

    - C.time.full

--- tst/cli-time.lua
+++ tst/cli-time.lua
@@ -26,4 +26,55 @@ end
 
 exec(ENV_EXE .. " chains rem cli-time")
 
+-- CONSOLIDATION
+do
+    print("==> Consolidation")
+
+    exec(ENV_EXE .. " --now=0 chains add cli-time dir " .. GEN_1P)
+
+    do
+        TEST "time-consolidation-24h"
+
+        exec (  -- 30 -> 29 (post)
+            ENV_EXE .. " --now=0 chain cli-time post inline 'p1' --sign " .. KEY
+        )
+        local out = exec(ENV_EXE .. " chain cli-time reps author " .. KEY)
+        assert(out == "29", "reps: " .. out)
+
+        exec (  -- refund P1 + consolidate P1 + cost P2 → 30
+            ENV_EXE .. " --now=86400 chain cli-time post inline 'p2' --sign " .. KEY
+        )
+        local out = exec(ENV_EXE .. " chain cli-time reps author " .. KEY)
+        assert(out == "30", "reps: " .. out)
+    end
+
+    exec(ENV_EXE .. " chains rem cli-time")
+
+    exec(ENV_EXE .. " --now=0 chains add cli-time dir " .. GEN_1P)
+
+    do
+        TEST "time-consolidation-1-per-day"
+
+        exec (  -- 30 -> 29
+            ENV_EXE .. " --now=0 chain cli-time post inline 'p1' --sign " .. KEY
+        )
+        exec (  -- refund P1 + cost P2 → 29
+            ENV_EXE .. " --now=0 chain cli-time post inline 'p2' --sign " .. KEY
+        )
+        exec (  -- refund P2 + cost P3 → 29
+            ENV_EXE .. " --now=0 chain cli-time post inline 'p3' --sign " .. KEY
+        )
+        local out = exec(ENV_EXE .. " chain cli-time reps author " .. KEY)
+        assert(out == "29", "reps: " .. out)
+
+        exec (  -- refund P3 + consolidate P1 only + cost P4 → 30
+            ENV_EXE .. " --now=86400 chain cli-time post inline 'p4' --sign " .. KEY
+        )
+        local out = exec(ENV_EXE .. " chain cli-time reps author " .. KEY)
+        assert(out == "30", "reps: " .. out)
+    end
+
+    exec(ENV_EXE .. " chains rem cli-time")
+end
+
 print("<== ALL PASSED")
