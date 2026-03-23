cbcf6d93b19aad3143d7347f8126f89cc78499f3
Francisco Sant'Anna <francisco.santanna@gmail.com>
2026-03-23 08:57:31 -0300

    - TODO: check apply()

--- src/freechains/chain/common.lua
+++ src/freechains/chain/common.lua
@@ -64,53 +64,73 @@ function apply (G, T)
         G.now = time
     end
 
-    if T == nil then
-        -- cap for query path
-        for k, v in pairs(G.authors) do
-            if v.reps > C.reps.max then
-                v.reps = C.reps.max
-            end
-        end
-        return true
-    end
-
-    if T.kind == 'post' then
-        G.xps = true
-        G.posts[T.hash] = {
-            author = T.sign,
-            time   = T.time,
-            state  = (T.beg and 'blocked') or (T.sign and '00-12') or 'blocked',
-            reps   = 0,
-        }
-        if T.sign then
-            G.xas = true
-            G.authors[T.sign] = G.authors[T.sign] or { reps=0 }
-            if not T.beg then
-                G.authors[T.sign].reps = G.authors[T.sign].reps - C.reps.cost
-                if G.authors[T.sign].time == nil then
-                    -- do not set for beg, bc not available to others
-                    G.authors[T.sign].time = T.time
+    if T then
+        -- validation
+        if T.kind == 'post' then
+            if T.sign and not T.beg then
+                local reps = G.authors[T.sign] and G.authors[T.sign].reps or 0
+                if reps <= 0 then
+                    return false, "insufficient reputation"
                 end
             end
+        elseif T.kind == 'like' then
+            if not T.sign then
+                return false, "unsigned"
+            end
+            if T.num == 0 then
+                return false, "expected positive integer"
+            end
+            if T.target ~= "post" and T.target ~= "author" then
+                return false, "target must be 'post' or 'author'"
+            end
+            if T.target == "post" and not G.posts[T.id] then
+                return false, "post not found"
+            end
+            local reps = G.authors[T.sign] and G.authors[T.sign].reps or 0
+            if reps <= 0 then
+                return false, "insufficient reputation"
+            end
         end
 
-    elseif T.kind == 'like' then
-        G.xas = true
-        G.authors[T.sign].reps = G.authors[T.sign].reps - math.abs(T.num)
-        local num = T.num * (100 - C.like.tax) // 100
-        if T.target == "post" then
-            local a = G.posts[T.id].author
-            G.authors[a] = G.authors[a] or { reps=0 }
-            G.authors[a].reps = G.authors[a].reps + num//C.like.split
-            G.posts[T.id].reps = G.posts[T.id].reps + num//C.like.split
-            if T.beg then
-                G.posts[T.id].state = "00-12"
-                G.posts[T.id].time = T.time
-            end
+        -- state mutation
+        if T.kind == 'post' then
             G.xps = true
-        else
-            G.authors[T.id] = G.authors[T.id] or { reps=0 }
-            G.authors[T.id].reps = G.authors[T.id].reps + num
+            G.posts[T.hash] = {
+                author = T.sign,
+                time   = T.time,
+                state  = (T.beg and 'blocked') or (T.sign and '00-12') or 'blocked',
+                reps   = 0,
+            }
+            if T.sign then
+                G.xas = true
+                G.authors[T.sign] = G.authors[T.sign] or { reps=0 }
+                if not T.beg then
+                    G.authors[T.sign].reps = G.authors[T.sign].reps - C.reps.cost
+                    if G.authors[T.sign].time == nil then
+                        -- do not set for beg, bc not available to others
+                        G.authors[T.sign].time = T.time
+                    end
+                end
+            end
+
+        elseif T.kind == 'like' then
+            G.xas = true
+            G.authors[T.sign].reps = G.authors[T.sign].reps - math.abs(T.num)
+            local num = T.num * (100 - C.like.tax) // 100
+            if T.target == "post" then
+                local a = G.posts[T.id].author
+                G.authors[a] = G.authors[a] or { reps=0 }
+                G.authors[a].reps = G.authors[a].reps + num//C.like.split
+                G.posts[T.id].reps = G.posts[T.id].reps + num//C.like.split
+                if T.beg then
+                    G.posts[T.id].state = "00-12"
+                    G.posts[T.id].time = T.time
+                end
+                G.xps = true
+            else
+                G.authors[T.id] = G.authors[T.id] or { reps=0 }
+                G.authors[T.id].reps = G.authors[T.id].reps + num
+            end
         end
     end
 
